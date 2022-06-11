eb9b52415e27089551d6dc1b613889d1dbbc154a
John McRae <johnfm7395@gmail.com>
2022-06-11 16:20:08 -0400

    V1 of category sum working

--- budget.py
+++ budget.py
@@ -20,18 +20,22 @@ import csv
 import itertools
 
 class Budget:
-    def readCash(input):
+    def __init__(self) -> None:
+        self.cashFlow = self.readCash('Cash-flow-2022-05-29-to-2022-07-02.csv')
+        self.categories = self.makeCategoryList(self.cashFlow)
+
+    def readCash(self, input):
         with open(input, newline='',encoding='utf-8-sig') as f:
             reader = csv.reader(f)
             data = list(reader)
         return data
    
-    cashFlow = readCash('Cash-flow-2022-05-29-to-2022-07-02.csv')
+    #cashFlow = readCash('Cash-flow-2022-05-29-to-2022-07-02.csv')
 
-    def makeCategoryList(self):
+    def makeCategoryList(self, cashFlow):
         cats = []
         # use itertools to loop from from the second row onwards
-        for rows in itertools.islice(Budget.cashFlow, 1, None):
+        for rows in itertools.islice(cashFlow, 1, None):
             exists = False
             # check if category is in list
             for items in cats:
@@ -39,25 +43,37 @@ class Budget:
                     exists = True
             if not exists:
                 cats.append(rows[2])
-        return cats
+        return sorted(cats)
+
+    #categories = makeCategoryList(cashFlow)
 
     def sumShared(self):
         sum = 0.0
-        for rows in Budget.cashFlow:
+        for rows in self.cashFlow:
             if rows[4] == 'y':
                 sum = sum + float(rows[1])
         return sum
    
     def sumNotShared(self):
         sum = 0.0
-        for rows in Budget.cashFlow:
+        for rows in self.cashFlow:
             if rows[4] == 'n':
                 sum = sum + float(rows[1])
         return sum
 
+    def sumByCategory(self):
+        print('Choose category:\n')
+        for cat in self.categories:
+            print(cat)
+        catChoice = input()
+        # sum values
+        sum = 0.0
+        for rows in self.cashFlow:
+            if rows[2] == catChoice:
+                sum = sum + float(rows[1])
+        return sum
+        
 b = Budget()
-
-print(b.makeCategoryList())
 
 while True:
     print('-- Budget app --\n' + 
@@ -68,6 +84,7 @@ while True:
     if int(menuChoice) == 2:
         print('Sum of not shared = ' + str(b.sumNotShared()) + '\n')
     if int(menuChoice) == 3:
-        pass
+        catSum = b.sumByCategory()
+        print('Sum = ' + str(catSum) + '\n')
     if int(menuChoice) == 0:
         break

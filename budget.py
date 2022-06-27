2ff300cb197a708ba153d5a0183e28d801f4614f
John McRae <johnfm7395@gmail.com>
2022-06-26 20:38:54 -0400

    Refactored budget to be a simpler, procedural script

--- budget.py
+++ budget.py
@@ -1,37 +1,19 @@
-"""
+'''
     budget.py calculates budget information
     The inputs to the budget script are a budget spreadsheet in csv format
     and a cash flow spreadsheet, also in csv format
 
-    methods:
-    - sum shared amounts
-    - sum not shared amounts
-    - list categories
-    - sum by category
-    - sort by date
-    - sort by amount
-    - display category
-    - display shared
-    - display not shared
-    - read cash flow data
-    - read budget
-"""
+    Inputs:
+    - budget csv
+    - cash flow csv
+
+    Output:
+    - sum and diff csv
+'''
 import csv
-import enum
 import itertools
 
-"""
-    The budget class reads the budget spreadsheet and
-    and compares those values to those in the cash flow
-
-    methods:
-    - read budget csv
-    - list primary categories
-    - list secondary categories`
-    - sum primary category goals
-    - diff categories with cash flow
-"""
-
+''' reused functions '''
 def makeCategoryList(array,index):
     cats = []
     # use itertools to loop from from the second row onwards
@@ -45,109 +27,46 @@ def makeCategoryList(array,index):
             cats.append(rows[index])
     return sorted(cats)
 
-class Budget:
-    def __init__(self) -> None:
-        self.budgetList = self.readBudget('Budget-2022-07.csv')
-        self.primaryList = makeCategoryList(self.budgetList,1)
-        self.primaryList = makeCategoryList(self.budgetList,2)
-
-    def readBudget(self, input):
-        with open(input, newline='',encoding='utf-8-sig') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-        return data
-
-    def sumPrimaryAmounts(self, budget, category):
+def sumByCategory(data,categoryList,categoryCol,valueCol):
+    categorySums = []
+    for category in categoryList:
         sum = 0
-        for rows in budget:
-            if budget[1] == category:
-                sum = sum + budget[0]
-        return sum
+        for rows in data:
+            if rows[categoryCol] == category:
+                sum = sum + float(rows[valueCol])
+        categorySums.append(sum)
+    return categorySums
 
+''' File paths '''
+# file paths
+budgetFile = 'Budget-2022-07.csv'
+cashFile   = 'Cash-flow-2022-05-29-to-2022-07-02.csv'
 
+''' Read files '''
+# read budget
+with open(budgetFile, newline='',encoding='utf-8-sig') as f:
+    reader = csv.reader(f)
+    budgetData = list(reader)
 
-class CashFlow:
-    def __init__(self) -> None:
-        self.cashFlow = self.readCash('Cash-flow-2022-05-29-to-2022-07-02.csv')
-        self.primaryCategories = makeCategoryList(self.cashFlow,2)
-        self.secondaryCategories = makeCategoryList(self.cashFlow,3)
+# read cash flow
+with open(cashFile, newline='',encoding='utf-8-sig') as f:
+    reader = csv.reader(f)
+    cashData = list(reader)
 
-    def readCash(self, input):
-        with open(input, newline='',encoding='utf-8-sig') as f:
-            reader = csv.reader(f)
-            data = list(reader)
-        return data
+''' Read categories '''
+budgetPrimaryCat   = makeCategoryList(budgetData,1)
+budgetSecondaryCat = makeCategoryList(budgetData,2)
+cashPrimaryCat     = makeCategoryList(cashData,  2)
+cashSecondaryCat   = makeCategoryList(cashData,  3)
 
-    def sumShared(self):
-        sum = 0.0
-        for rows in self.cashFlow:
-            if rows[5] == 'y':
-                sum = sum + float(rows[1])
-        return sum
-   
-    def sumNotShared(self):
-        sum = 0.0
-        for rows in self.cashFlow:
-            if rows[5] == 'n':
-                sum = sum + float(rows[1])
-        return sum
+''' Sum by category '''
+budgetPrimarySum = sumByCategory(budgetData,budgetPrimaryCat,1,0)
+cashPrimarySum   = sumByCategory(cashData,cashPrimaryCat,2,1)
+cashSecondarySum = sumByCategory(cashData,cashSecondaryCat,3,1)
 
-    def sumByCategory(self,catList,rowIdx):
-        print('Choose category:\n')
-        # add index using enumerate()
-        for idx, cat in enumerate(catList):
-            print(str(idx + 1) + ". " + cat, end = "\t")
-        print("\n")
-        catChoice = catList(int(input()) - 1)
-        # sum values
-        sum = 0.0
-        for rows in self.cashFlow:
-            if rows[rowIdx] == catChoice:
-                sum = sum + float(rows[1])
-        return catChoice, sum
+''' diff by category '''
 
-        
-b = CashFlow()
+''' print data to csv '''
 
-class userInterface():
-    def __init__(self,Budget,CashFlow) -> None:
-        self.budget = Budget
-        self.cash   = CashFlow
-
-    def homePage():
-        print('\n=================== BUDGET CALCULATOR ===================\n\n' + 
-            '1. Choose files   2. Display files   3. Show sums  0. Exit')
-        menuChoice = input()
-        if int(menuChoice) == 0:
-            return False
-        elif int(menuChoice) == 3:
-            pass # sumPage
-        else:
-            return False
-
-    def sumPage():
-        print('\n------------------------ Show Sums ------------------------\n\n' + 
-            '1. Sum total cash flow  2. Sum by primary category  3. Sum by secondary category')
-        menuChoice = input()
-        if int(menuChoice) == 1:
-            pass
-        
-        
-
-
-runUI = True
-while runUI == True:
-    # print('-- Budget app --\n' + 
-    #     '1. Sum shared  2. Sum not shared   3. Sum by category  0. Exit')
-    # menuChoice = input()
-    # if int(menuChoice) == 1:
-    #     print('Sum of shared = ' + str(b.sumShared()) + '\n')
-    # if int(menuChoice) == 2:
-    #     print('Sum of not shared = ' + str(b.sumNotShared()) + '\n')
-    # if int(menuChoice) == 3:
-    #     catSum = b.sumByCategory()
-    #     print('Sum = ' + str(catSum) + '\n')
-    # if int(menuChoice) == 0:
-    #     break
-
-    runUI = userInterface()
+# for debug
+pass
